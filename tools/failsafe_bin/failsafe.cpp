f3a46cbace9d662528cfdd29797181faa08097ed
Derek Benda <sleepytonic@gmail.com>
2016-10-23 17:05:59 -0400

    Bring back tools and misc files
    This reverts commit 2ff89d6b0c55d086aca6a57b99a93b5c0e7593aa.

---
+++ tools/failsafe_bin/failsafe.cpp
@@ -0,0 +1,26 @@
+/*
+	Compile: particle compile electron failsafe.cpp
+	Upload: particle serial --flash electron_firmware_xxxx.bin or,
+		particle serial --flash electron_failsafe.bin
+	Usage:
+		particle serial monitor
+*/
+
+// Global variables
+int ON_BOARD_LED = D7;
+
+void setup(){
+	pinMode(ON_BOARD_LED, OUTPUT);
+	Serial.begin(9600);
+}
+
+void loop() {
+
+	Serial.println("LED ON");
+	digitalWrite(ON_BOARD_LED, HIGH);
+	delay(5000);
+
+	Serial.println("LED OFF");
+	digitalWrite(ON_BOARD_LED, LOW);
+	delay(5000);
+}
